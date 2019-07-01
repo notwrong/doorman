2f984a7371fc3260e0f3dd4ba3e34cec1776e731
Gannon <42745542+GannonDetroit@users.noreply.github.com>
2019-07-01 13:12:18 -0400

    removed HelloWorld Component and it's import, was useless

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <v-app>
     <v-content>
-      <Home/>
+      <Home />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
 import Home from "./views/Home.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
     Home
   },
   data() {
