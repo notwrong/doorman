9b74fe5823ed5e0c5862bc8c247309c7def9a983
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-07-01 18:19:52 -0400

    Adds welcome message and removes Login button after login

--- src/components/LandingPage/AppBar.vue
+++ src/components/LandingPage/AppBar.vue
@@ -3,13 +3,26 @@
     <v-toolbar-title ripple class="primary--text">Doorman</v-toolbar-title>
     <v-spacer></v-spacer>
     <v-btn flat color="primary">
-      <span>Login</span>
+      <span v-if="!this.$store.state.currentUser" @click="login">Login</span>
+      <span v-else @click="logUser">Welcome, {{ getFirstName() }}</span>
       <v-icon right>account_circle</v-icon>
     </v-btn>
   </v-toolbar>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    login() {
+      this.$store.dispatch("githubLogin");
+    },
+    logUser() {
+      console.log(this.$store.state.currentUser);
+    },
+    getFirstName() {
+      return this.$store.state.currentUser.name.split(" ")[0];
+    }
+  }
+};
 </script>
 
