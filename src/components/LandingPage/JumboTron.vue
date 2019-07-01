6884566641231c9e6182f0e517c37ae390d82ebc
Thomas Hessburg <44884308+TomHessburg@users.noreply.github.com>
2019-07-01 15:42:50 -0400

    initial jumbotron layout

--- src/components/LandingPage/JumboTron.vue
+++ src/components/LandingPage/JumboTron.vue
@@ -3,19 +3,24 @@
     <div class="jumbowraper">
       <div class="jumbowrapertop">
         <h1 class="display-4 pa-4 primary--text font-weight-thin grey--text text--darken-1">Doorman</h1>
+
         <h3
           class="headline pa-4 secondary--text font-weight-thin grey--text text--lighten-3"
         >A personal assistant to manage your github invites</h3>
       </div>
       <div class="jumbowrapperbottom">
-        <v-btn flat class="primary outlined">About</v-btn>
-        <v-btn flat outline class="grey grey--text text--lighten-2">Sign Up</v-btn>
+        <v-btn large flat class="primary outlined">About</v-btn>
+        <v-btn large flat outline class="grey grey--text text--lighten-2">Sign Up</v-btn>
       </div>
+      <!-- <img class="github-fork" src="../../assets/githubfork.png" alt="github for"> -->
     </div>
   </v-container>
 </template>
 
 <script>
+// import octicons from "@primer/octicons";
+// console.log(octicons["git-pull-request"].path);
+
 export default {
   data: () => ({}),
   props: {}
@@ -25,6 +30,9 @@ export default {
 <style scoped>
 .jumbowraper {
   padding: 8px;
+  padding-top: 40px;
+  padding-bottom: 80px;
+  margin-top: 0;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -35,6 +43,10 @@ export default {
   flex-direction: column;
   justify-content: center;
   align-items: center;
+}
+.github-fork {
+  width: 150px;
+  opacity: 0.1;
 }
 </style>
 
