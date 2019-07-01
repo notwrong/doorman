0d92531842dec29e7d0a1f60197e35c0eff8aba8
Thomas Hessburg <44884308+TomHessburg@users.noreply.github.com>
2019-07-01 14:55:16 -0400

    edits to nav bar and jumbo tron

--- src/components/LandingPage/AppBar.vue
+++ src/components/LandingPage/AppBar.vue
@@ -1,8 +1,13 @@
 <template>
   <v-toolbar app flat fixed clipped-left>
-    <v-toolbar-title ripple class="primary--text">Doorman</v-toolbar-title>
+    <v-toolbar-title ripple class="primary--text">
+      <v-btn flat>
+        <span class="primary--text title">Doorman</span>
+        <v-icon right large class="primary--text">code</v-icon>
+      </v-btn>
+    </v-toolbar-title>
     <v-spacer></v-spacer>
-    <v-btn flat color="primary">
+    <v-btn flat color="grey">
       <span>Login</span>
       <v-icon right>account_circle</v-icon>
     </v-btn>
--- src/components/LandingPage/JumboTron.vue
+++ src/components/LandingPage/JumboTron.vue
@@ -1,7 +1,17 @@
 <template>
   <v-container app>
-    <h1>Doorman</h1>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit voluptatum blanditiis aliquam esse dolore! Ipsum fugit quasi quo quisquam ullam! Provident minus sequi amet nihil iure totam, blanditiis incidunt magni?</p>
+    <div class="jumbowraper">
+      <div class="jumbowrapertop">
+        <h1 class="display-4 pa-4 primary--text font-weight-thin grey--text text--darken-1">Doorman</h1>
+        <h3
+          class="headline pa-4 secondary--text font-weight-thin grey--text text--lighten-3"
+        >A personal assistant to manage your github invites</h3>
+      </div>
+      <div class="jumbowrapperbottom">
+        <v-btn flat class="primary outlined">About</v-btn>
+        <v-btn flat outline class="grey grey--text text--lighten-2">Sign Up</v-btn>
+      </div>
+    </div>
   </v-container>
 </template>
 
@@ -13,6 +23,19 @@ export default {
 </script>
 
 <style scoped>
+.jumbowraper {
+  padding: 8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.jumbowrapertop {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 </style>
 
 
