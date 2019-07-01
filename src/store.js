59723651ea60e04de5c2cd5d893aaa8d0195573d
ryanboris <ryan.boris@gmail.com>
2019-07-01 15:21:31 -0400

    :bug: Correct passing of vuex state

--- src/store.js
+++ src/store.js
@@ -11,7 +11,7 @@ export default new Vuex.Store({
   },
   mutations: {
     setCurrentUser(state, user) {
-      this.state.currentUser = user;
+      state.currentUser = user;
     }
   },
   actions: {
