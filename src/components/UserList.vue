0936ae0875fd8bb0c37328720091e4bebcc5ef22
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-07-01 11:56:56 -0500

    :sparkles: User will have symbol denoting blocked/allowed
    Dummy data added until store gets set. Each user will display a symbol denoting if they are blocked or allowed -- or none, if neither, by logged in user (also dummy data)

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -3,13 +3,15 @@
     <h1 class="primary--text">Github Users</h1>
 
     <v-container class="my-5">
-      <v-card class="pa-2">
+      <v-card class="pa-2 my-3" v-for="(user, i) in users" :key="i">
         <v-layout row justify-space-between>
           <v-flex xs10 md6>
-            <div class="pt-1">Username</div>
+            <v-icon left>Github</v-icon>
+            <div class="pt-1">Username: {{user.username}}</div>
           </v-flex>
           <v-flex xs1 md1>
-            <v-icon right class="error--text">block</v-icon>
+            <v-icon v-if="isAllowed(user)" right class="success--text">check</v-icon>
+            <v-icon v-if="isBlocked(user)" right class="error--text">block</v-icon>
           </v-flex>
         </v-layout>
       </v-card>
@@ -21,11 +23,22 @@
 export default {
   data() {
     return {
-      user: {
+      authUser: {
         username: "DaftBeowulf",
         creds: { token: "token" },
-        allow: {},
-        block: {},
+        allow: {
+          12345: {
+            username: "nickcannariato"
+          },
+          27734: {
+            username: "GannonDetroit"
+          }
+        },
+        block: {
+          28768: {
+            username: "TomHessburg"
+          }
+        },
         user_id: "FK_TO_FIREBASE_USER_AUTH_COLLECTION"
       },
       users: [
@@ -35,10 +48,49 @@ export default {
           allow: {},
           block: {},
           user_id: "FK_TO_FIREBASE_USER_AUTH_COLLECTION"
+        },
+        {
+          username: "TomHessburg",
+          creds: { token: "token" },
+          allow: {},
+          block: {},
+          user_id: "FK_TO_FIREBASE_USER_AUTH_COLLECTION"
+        },
+        {
+          username: "GannonDetroit",
+          creds: { token: "token" },
+          allow: {},
+          block: {},
+          user_id: "FK_TO_FIREBASE_USER_AUTH_COLLECTION"
+        },
+        {
+          username: "ajb85",
+          creds: { token: "token" },
+          allow: {},
+          block: {},
+          user_id: "FK_TO_FIREBASE_USER_AUTH_COLLECTION"
+        },
+        {
+          username: "ryanboris",
+          creds: { token: "token" },
+          allow: {},
+          block: {},
+          user_id: "FK_TO_FIREBASE_USER_AUTH_COLLECTION"
         }
       ]
     };
-  }
+  },
+  methods: {
+    isAllowed(user) {
+      const list = Object.values(this.authUser.allow);
+      return Boolean(list.find(u => u.username === user.username));
+    },
+    isBlocked(user) {
+      const list = Object.values(this.authUser.block);
+      return Boolean(list.find(u => u.username === user.username));
+    }
+  },
+  computed: {}
 };
 </script>
 
