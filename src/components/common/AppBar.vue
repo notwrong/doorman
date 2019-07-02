c4130cac4ada78cfbcf6ac3eb275f31a054c6e5d
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-07-02 17:43:22 -0500

    :sparkles: Added app bar link to dashboard

--- src/components/common/AppBar.vue
+++ src/components/common/AppBar.vue
@@ -5,11 +5,12 @@
         <!-- <span class="primary--text title">Doorman</span>
         <v-icon right large class="primary--text">code</v-icon>-->
         <!-- <img style="width: 130px;" src="../../assets/doorman-logo-alternate.svg" alt> -->
-        <DoormanLogo/>
+        <DoormanLogo />
         <!-- i know theres a way to do the fill for these in CSS but it was giving me crap so I didnt waste time haha -->
       </v-btn>
     </v-toolbar-title>
     <v-spacer></v-spacer>
+    <v-btn flat color="grey" :to="{name: 'dashboard'}">Dashboard</v-btn>
     <v-btn flat color="grey" :to="{name:'about'}">About Us</v-btn>
     <v-btn flat v-if="!currentUser" @click="githubLogin" color="grey">
       <span>Login</span>
@@ -17,8 +18,8 @@
     </v-btn>
     <v-btn flat v-else @click="logUser" color="primary">
       <span>Welcome, {{ firstName }}</span>
-      <img class="avatar" :src="currentUser.avatar_url" alt="User avatar">
-      <img>
+      <img class="avatar" :src="currentUser.avatar_url" alt="User avatar" />
+      <img />
     </v-btn>
   </v-toolbar>
 </template>
