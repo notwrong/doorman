2e8a62ee178025502ba0e83b4f3487a1e23dc71d
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-07-02 22:29:18 -0400

    installs fontawesome icons

--- src/main.js
+++ src/main.js
@@ -8,6 +8,15 @@ import { auth } from "./utils/firebaseConfig";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faUserTimes, faUserCheck } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faUserTimes, faUserCheck);
+library.add(faUserSecret);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 let app = null;
