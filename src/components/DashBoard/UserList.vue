64d31ed94f04061f23031b9ef962cf0dcb1af072
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-07-03 12:31:08 -0500

    :bug: Fixes bug with filter options loading
    Filter options now only display if user is currently logged in and the array of blocked + allowed users has length, preventing interaction before they've added somebody. Also changed the 'your list is empty' message to display if the list is empty, not just if the person isn't logged in

--- src/components/DashBoard/UserList.vue
+++ src/components/DashBoard/UserList.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app class="users-list" dark>
     <v-container class="my-5">
-      <v-btn-toggle v-if="returnFilter.length>0" v-model="filter">
+      <v-btn-toggle v-if="currentUser && returnFilter.length>0" v-model="filter">
         <v-btn flat value="all" class="primary secondary--text">All</v-btn>
         <v-btn flat value="allow" class="primary secondary--text">Accepted</v-btn>
         <v-btn flat value="block" class="primary secondary--text">Declined</v-btn>
@@ -61,6 +61,8 @@ export default {
           return this.blockedList;
         case "allow":
           return this.allowedList;
+        default:
+          return [];
       }
     }
   }
